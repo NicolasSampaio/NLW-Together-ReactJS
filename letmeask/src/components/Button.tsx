800729050f0f0cab53c30989adda605a9e7d2df9
Nicolas Sampaio <nicolas.pereira.sampaio16@gmail.com>
2021-06-20 23:29:15 -0300

    ReactJS NLW Together - Aula 1 - Liftoff
    Projected created using react-create-app
    Firebase configuration.
    Learned Componentization.

---
+++ letmeask/src/components/Button.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+
+export function Button() {
+    //let counter = 0;
+    const [counter, setCounter] = useState(0);
+
+
+    function increment(){
+        //counter += 1;
+        setCounter(counter + 1);
+        console.log(counter);
+    }
+
+
+    return (
+        <button onClick={increment}>{counter}</button>
+    )
+}
