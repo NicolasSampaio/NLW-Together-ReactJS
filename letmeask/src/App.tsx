e5500c0f2c38ccc9a2aae0e8fb408b00c0a0d7ac
Nicolas Sampaio <nicolas.pereira.sampaio16@gmail.com>
2021-06-22 23:22:43 -0300

    ReactJS NLW Together - Aula 2 - Maximum Speed
    Aprendizado de context, effect, route e autenticação
    Use of context, effect and route learned. Authentication with firebase developed.

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,18 +1,19 @@
-import { Button } from './components/Button'
+import { Route, BrowserRouter } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
-  return (
-    <div>
-      <Button></Button>      
-      <Button></Button>      
-      <Button></Button>      
-      <Button></Button>      
-      <Button></Button>      
-      <Button></Button>      
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<AuthContextProvider>
+				<Route path="/" exact={true} component={Home} />
+				<Route path="/rooms/new" component={NewRoom} />
+			</AuthContextProvider>
+		</BrowserRouter>
+	);
 }
 
 export default App;
-
-
